5d2683406333f130ebbf31ed3ac18c4dd3c69003
André Hoffmann <antihero2006@gmail.com>
2012-05-15 17:52:03 +0200

    Created wrapper that tests the existence of symlinks on unix/mac.

---
+++ scripts/is_symlink.m
@@ -0,0 +1,16 @@
+function [ status ] = is_symlink( link )
+%Checks if file is a symbolic link
+%   returns 1 if symlink or on windows
+%   0 otherwise
+
+%% Don't do anything on windows
+if (ispc())
+    status = 0;
+    return 
+end
+
+%% Check for symlink
+status = unix(sprintf('readlink %s', link)) == 0;
+
+end
+
