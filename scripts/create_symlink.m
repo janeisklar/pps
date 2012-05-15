96b9f1aabf10e1f105ab1752f83fdace217ca472
André Hoffmann <antihero2006@gmail.com>
2012-05-15 17:41:18 +0200

    Added simple wrapper to create symbolic link on unix/mac-systems.

---
+++ scripts/create_symlink.m
@@ -0,0 +1,16 @@
+function [ status ] = create_symlink(source, target)
+%Creates a symlink from target to source
+%   does nothing on windows
+%   returns true if successful
+
+%% Don't do anything on windows
+if (ispc())
+    status = 0;
+    return 
+end
+
+%% Create the symlink
+status = unix(sprintf('ln -s %s %s', source, target)) == 0;
+
+end
+
