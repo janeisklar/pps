46d5d8b7c9e75fc1cad3405957bb37ddf49f2955
André Hoffmann <antihero2006@gmail.com>
2012-05-16 17:20:02 +0200

    Added utility function that returns the file count of a tar archive.

---
+++ scripts/get_tar_file_count.m
@@ -0,0 +1,16 @@
+function [ count ] = get_tar_file_count( tarPath )
+% Returns the number of files in a tar file
+%   Returns 99999 on Windows
+
+%% Don't do anything on Windows
+if (ispc())
+    count = 99999;
+    return
+end
+
+%% Use the shell tar utility to list the files in the archive and count them
+[~, r]  = unix(sprintf('tar tf "%s" | grep -i .ima | wc -l', tarPath));
+count   = str2num(strtrim(r));
+
+end
+
