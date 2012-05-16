f6c9204331be85d2837e70ef3d0154692587c9b4
André Hoffmann <antihero2006@gmail.com>
2012-05-16 17:26:43 +0200

    Added function that adds a DICOM archive if it hasn't been created already

---
+++ scripts/process_scan.m
@@ -0,0 +1,11 @@
+function [ output_args ] = process_scan( scanDir )
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+%% Check presence of niftis
+
+
+%% Check presence of DICOM tar archive
+process_scan_archive(scanDir);
+    
+end
