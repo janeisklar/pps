e35805dc75b80fd9672b064f56061fd96b254efe
André Hoffmann <antihero2006@gmail.com>
2013-04-22 17:22:20 +0200

    FINDINGS.fmri is not created when there are no errors

--- ppUpdateFindingsLog.m
+++ ppUpdateFindingsLog.m
@@ -1,8 +1,18 @@
-function [ paradigmPath ] = ppUpdateFindingsLog(measurementPath)
+function [] = ppUpdateFindingsLog(measurementPath)
 %% Updates findings.fmri to represent the new error count
 
-measurementPath  = ppGetFullPathTrailing(measurementPath);
+measurementPath = ppGetFullPathTrailing(measurementPath);
+findingsLogPath = strcat(measurementPath, 'FINDINGS.fmri');
 
-unix(sprintf('echo `find %s -name ERROR.fmri -print |wc -l`" Errors, 0 Warnings" > %sFINDINGS.fmri', measurementPath, measurementPath));
+[unused,nErrors]= unix(sprintf('find %s -name "ERROR.fmri" | wc -l', measurementPath));
+nErrors         = str2num(nErrors)
+
+if nErrors > 0
+    unix(sprintf('echo "%d Errors, 0 Warnings" > %s', nErrors, findingsLogPath));
+else
+    if ( exist(findingsLogPath, 'file') )
+      delete(findingsLogPath);
+    end
+end
 
 end
